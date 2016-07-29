ceb3f835da268ad7e9c909eb7153cceb9cb4edf4
Aldridge Braz <aldridge.braz@gmail.com>
2016-07-29 16:33:25 -0400

    Detect usernames
    Support backend to keep track of unique users and broadcast username
    when user enters/leaves chat

--- index.js
+++ index.js
@@ -5,6 +5,47 @@ var http = require("http").Server(app);
 
 var io = require("socket.io")(http);
 
+var user_list = null;
+
+function User(name, id) {
+	this.username = name;
+	this.id = id;
+	this.next = null;
+}
+
+function add_user(user_node) {
+	if (user_list == null) {
+		user_list = user_node;
+	}
+	else {
+		user_node.next = user_list;
+		user_list = user_node;
+	}
+}
+
+function remove_user(id) {
+	var prev = null;
+	var current = user_list;
+	while (current != null) {
+		if (id == current.id) {
+			if (current == user_list) {
+				user_list = current.next;
+				current.next = null;
+				return current.username;
+			}
+			else {
+				prev = current.next;
+				current.next = null;
+				return current.username;
+			}
+		}
+		prev = current;
+		current = current.next;
+	}
+	return null;
+}
+
+
 app.use(express.static(__dirname + "/public"));
 
 app.use(express.static(__dirname + "/node_modules"));
@@ -16,13 +57,34 @@ app.get("/", function(req, res) {
 io.on("connection", function(socket) {
 	console.log("A user connected");
 
+	socket.on("new user", function(username) {
+		
+		console.log("username: " + username);
+		console.log("socket.id: " + socket.id);
+		
+		var new_user = new User(username, socket.id);
+		
+		socket.broadcast.emit("new user", username);
+		
+		
+		add_user(new_user);
+	});
+	
 	socket.on("chat message", function (msg) {
 		console.log("message: " + msg);
-		io.emit("chat message", msg);
+		socket.broadcast.emit("chat message", msg);
 	});
+	
 	socket.on("disconnect", function() {
 		console.log("User disconnected");
+		var username = remove_user(socket.id);
+		
+		console.log("username: " + username);
+		console.log("socket.id: " + socket.id);
+		
+		io.emit("user left", username);
 	});
+	
 });
 
 http.listen(3000, function() {
